0d1f296bf23f223335ed5b38272b763ded7234d4
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2025-06-18 09:30:48 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,37 +8,41 @@ import os
 # 📂 Reemplaza por el ID de tu carpeta en Google Drive
 FOLDER_ID = "10ZQjPf5cmva2uUqpSG8QR1uznGVnaL4O"
 
-@st.cache_resource
-def get_drive_service():
-    creds = service_account.Credentials.from_service_account_info(
-        st.secrets["google_service_account"],
-        scopes=["https://www.googleapis.com/auth/drive"]
-    )
-    return build("drive", "v3", credentials=creds)
+st.write(st.secrets)
 
-drive_service = get_drive_service()
 
-st.title("📤 Subir archivo Excel a Google Drive")
 
-uploaded_file = st.file_uploader("Selecciona un archivo Excel", type=["xlsx", "xls"])
+# @st.cache_resource
+# def get_drive_service():
+#     creds = service_account.Credentials.from_service_account_info(
+#         st.secrets["google_service_account"],
+#         scopes=["https://www.googleapis.com/auth/drive"]
+#     )
+#     return build("drive", "v3", credentials=creds)
 
-if uploaded_file:
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".xlsx") as tmp:
-        tmp.write(uploaded_file.read())
-        temp_path = tmp.name
+# drive_service = get_drive_service()
 
-    file_metadata = {
-        "name": uploaded_file.name,
-        "parents": [FOLDER_ID]
-    }
-    media = MediaFileUpload(temp_path, mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
-    uploaded = drive_service.files().create(
-        body=file_metadata,
-        media_body=media,
-        fields="id, webViewLink"
-    ).execute()
+# st.title("📤 Subir archivo Excel a Google Drive")
 
-    os.remove(temp_path)
+# uploaded_file = st.file_uploader("Selecciona un archivo Excel", type=["xlsx", "xls"])
 
-    st.success("✅ Archivo subido correctamente a Google Drive.")
-    st.markdown(f"🔗 [Ver archivo en Drive]({uploaded['webViewLink']})")
+# if uploaded_file:
+#     with tempfile.NamedTemporaryFile(delete=False, suffix=".xlsx") as tmp:
+#         tmp.write(uploaded_file.read())
+#         temp_path = tmp.name
+
+#     file_metadata = {
+#         "name": uploaded_file.name,
+#         "parents": [FOLDER_ID]
+#     }
+#     media = MediaFileUpload(temp_path, mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+#     uploaded = drive_service.files().create(
+#         body=file_metadata,
+#         media_body=media,
+#         fields="id, webViewLink"
+#     ).execute()
+
+#     os.remove(temp_path)
+
+#     st.success("✅ Archivo subido correctamente a Google Drive.")
+#     st.markdown(f"🔗 [Ver archivo en Drive]({uploaded['webViewLink']})")
